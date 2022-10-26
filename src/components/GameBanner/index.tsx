f4d69e470b439b525f5b858a91838df798a65804
vcfernandes <vinnerr02@hotmail.com>
2022-10-25 21:12:29 -0400

    substituindo componentes da interface

--- src/components/GameBanner/index.tsx
+++ src/components/GameBanner/index.tsx
@@ -12,13 +12,13 @@ interface GameBannerProps{
  *          atributos estáticos das imagem (src), titulo e contador de anuncios pelas respectivas props
  */
 
-export function GameBanner() {
+export function GameBanner(props: GameBannerProps) {
   return (<><a href="" className="relative rounded-lg overflow-hidden">
-  <img src="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-188x250.jpg" alt="" />
+  <img src={props.id} alt="" />
 
     <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-      <strong className="font-bold text-white block">GTA V</strong>
-      <span className="text-zinc-300 text-sm block">3 anuncio(s)</span>
+    <strong className="font-bold text-white block">{props.title}</strong>
+      <span className="text-zinc-300 text-sm block">{props.adsCount} anuncio(s)</span>
     </div>
   </a></>);
 }
